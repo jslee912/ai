3ddba9fac8952d25b3cf5d4d9d49a99ec71cac43
jslee912 <jslee912@gmail.com>
2025-09-19 18:01:51 +0900

    튜플,딕셔너리,셋

--- source/01_python/lst.py
+++ source/01_python/lst.py
@@ -4,3 +4,10 @@ def safe_index(lst, item):
     if item in lst:
         return lst.index(item)
     return -1
+def safe_index(lst, item, start=0):    
+    """
+    첫번째 매개변수 lst : 리스트, 튜플
+    두번째 매개변수 item : 리스트나 튜플에서 찾을 데이터
+    세번째 매개변수 start : 몇번째 index부터 찾을지 index수(기본값: 0)
+    """
+    return lst.index(item, start) if item in lst[start:] else -1
