5321700fd50bf50692ec078dc882710faab731ef
jslee912 <jslee912@gmail.com>
2025-11-04 17:59:36 +0900

    js 객체

---
+++ source/03_JavaScript/ch05_함수/sumAll_sam.js
@@ -0,0 +1,16 @@
+function sumAll(){
+  // 매개변수가 없으면 -999 return / 매개변수 있으면 매개변수 합 return
+  let resultSum = 0;
+  if(arguments.length==0){
+    return -999;
+  }else{
+    for(let data of arguments){
+      resultSum += data;
+    }
+  }
+  return resultSum;
+}
+//=== 테스트 코드 ===
+// console.log(sumAll());
+// console.log(sumAll(1,3,5,7));
+// console.log(sumAll(7));
